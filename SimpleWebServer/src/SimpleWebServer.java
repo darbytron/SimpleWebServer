3cfb6a148c4d1a7925aa35352cd342e96bc1cdc7
Tyler Darby <me@tylerdarby.com>
2015-09-25 20:15:23 -0400

    Adding logs for clarity:

--- SimpleWebServer/src/SimpleWebServer.java
+++ SimpleWebServer/src/SimpleWebServer.java
@@ -28,61 +28,70 @@ public class SimpleWebServer {
  	dServerSocket = new ServerSocket (PORT);          
     }                                                     
  
-    public void run() throws Exception {                 
-	while (true) {                                   
- 	    /* wait for a connection from a client */
- 	    Socket s = dServerSocket.accept();           
- 
- 	    /* then process the client's request */
- 	    processRequest(s);                           
- 	}                                                
+    public void run() throws Exception {  
+    	System.out.println("Listening for requests");
+		while (true) {                                   
+	 	    /* wait for a connection from a client */
+	 	    Socket s = dServerSocket.accept();           
+	 
+	 	    /* then process the client's request */
+	 	    processRequest(s);                           
+	 	}                                                
     }                                                    
  
     /* Reads the HTTP request from the client, and
        responds with the file the user requested or
        a HTTP error code. */
     public void processRequest(Socket s) throws Exception { 
- 	/* used to read data from the client */ 
- 	BufferedReader br =                                 
- 	    new BufferedReader (
-				new InputStreamReader (s.getInputStream())); 
- 
- 	/* used to write data to the client */
- 	OutputStreamWriter osw =                            
- 	    new OutputStreamWriter (s.getOutputStream());  
-     
- 	/* read the HTTP request from the client */
- 	String request = br.readLine();                    
- 
- 	String command = null;                             
- 	String pathname = null;                            
-     
- 	/* parse the HTTP request */
- 	StringTokenizer st = 
-	    new StringTokenizer (request, " ");               
- 
- 	command = st.nextToken();                       
- 	pathname = st.nextToken();                      
- 
-	if (command.equals("GET")) {                    
-	    /* if the request is a GET
-	       try to respond with the file
-	       the user is requesting */
-	    serveFile (osw,pathname);                   
- 	}                                              
- 	else {                                         
-	    /* if the request is a NOT a GET,
-	       return an error saying this server
-	       does not implement the requested command */
-	    osw.write ("HTTP/1.0 501 Not Implemented\n\n");
- 	}                                               
- 	
- 	/* close the connection to the client */
- 	osw.close();                                    
+    	System.out.println("Request received. Processing...");
+	 	/* used to read data from the client */ 
+	 	BufferedReader br =                                 
+	 	    new BufferedReader (
+					new InputStreamReader (s.getInputStream())); 
+	 
+	 	/* used to write data to the client */
+	 	OutputStreamWriter osw =                            
+	 	    new OutputStreamWriter (s.getOutputStream());  
+	     
+	 	/* read the HTTP request from the client */
+	 	String request = br.readLine();                    
+	 
+	 	String command = null;                             
+	 	String pathname = null;                            
+	    
+	 	/* parse the HTTP request */
+	 	StringTokenizer st = 
+		    new StringTokenizer (request, " ");               
+	 
+	 	command = st.nextToken();                       
+	 	pathname = st.nextToken();                      
+	 
+	 	System.out.println("Process parced. Analyzing command...");
+		if (command.equals("GET")) {                    
+		    /* if the request is a GET
+		       try to respond with the file
+		       the user is requesting */
+		    serveFile (osw,pathname);                   
+	 	} else if(command.equals("PUT")) {
+	 		/*	if the request is a PUT 
+	 		 	try to update the file 
+	 		 	the user is specifying. 
+	 		 */
+	 		updateFile(osw, pathname);
+	 		
+	 	}                 
+	 	else {                                         
+		    /* if the request is a NOT a GET,
+		       return an error saying this server
+		       does not implement the requested command */
+		    osw.write ("HTTP/1.0 501 Not Implemented\n\n");
+	 	}                                               
+	 	
+	 	/* close the connection to the client */
+	 	osw.close();                                    
     }                                                   
  
-    public void serveFile (OutputStreamWriter osw,      
-			   String pathname) throws Exception {
+    public void serveFile (OutputStreamWriter osw, String pathname) throws Exception {
  	FileReader fr=null;                                 
  	int c=-1;                                           
  	StringBuffer sb = new StringBuffer();
@@ -120,12 +129,17 @@ public class SimpleWebServer {
  	osw.write (sb.toString());                                  
     }                                                       
  
+    public void updateFile(OutputStreamWriter osw, String pathname) throws Exception {
+    	
+    }
     /* This method is called when the program is run from
        the command line. */
     public static void main (String argv[]) throws Exception { 
- 
- 	/* Create a SimpleWebServer object, and run it */
- 	SimpleWebServer sws = new SimpleWebServer();           
- 	sws.run();                                             
+    	System.out.println("Starting server...");
+    	/* Create a SimpleWebServer object, and run it */
+    	SimpleWebServer sws = new SimpleWebServer();           
+    	sws.run();
+    	System.out.println("Server running...");
+ 	
     }                                                          
 }                                                              
